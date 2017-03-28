e3f1e119bc2b2d76a2d2a0400d1a1aafc7df36eb
Wi-SUN <paduffy@cisco.com>
2017-03-28 03:00:34 +0000

    Generated code by SwaggerHub

--- nodejs-server/controllers/DefaultService.js
+++ nodejs-server/controllers/DefaultService.js
@@ -60,7 +60,22 @@ exports.securityKeysGET = function(args, res, next) {
   /**
    * parameters expected in the args:
   **/
-  // no response value expected for this operation
-  res.end();
+    var examples = {};
+  examples['application/json'] = {
+  "gtk0" : "aeiou",
+  "gtk1" : "aeiou",
+  "gtk2" : "aeiou",
+  "gtk3" : "aeiou",
+  "ptk" : "aeiou",
+  "pmk" : "aeiou"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
 }
 
